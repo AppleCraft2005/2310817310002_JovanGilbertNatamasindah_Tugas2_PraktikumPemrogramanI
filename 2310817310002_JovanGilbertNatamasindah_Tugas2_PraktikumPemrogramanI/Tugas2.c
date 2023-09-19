b9aa09c9434fa4e472378fd482bdd449cae57771
Jovan Gilbert N <141586230+AppleCraft2005@users.noreply.github.com>
2023-09-19 19:27:47 +0800

    Add files via upload

---
+++ 2310817310002_JovanGilbertNatamasindah_Tugas2_PraktikumPemrogramanI/Tugas2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void main() {
+    char SALAM[] = "Selamat Pagi,";
+    char TI[] = "Teknologi Informasi FTULM Selalu dihati";
+
+    printf("%s\n\n", SALAM);
+    printf("%s", TI);
+
+}
